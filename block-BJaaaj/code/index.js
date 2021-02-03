7de7985dd4ce92dadfb4b279caba44f9fc063960
priyabadatiya <priyabadatiya@gmail.com>
2021-02-03 17:58:59 +0530

    Accesing Assigning variables

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,29 +6,45 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
-// Alert the value of the lastName.
-// Assign a new value of `"John"` to firstName.
-// Assign a new value of `"Snow"` to lastName.
-// Alert the new value of `firstName` and `lastName` by concatenating them
-// Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-
-// Declare three variables `name,` `newUser` and `isAdmin`.
+alert(firstName)
+    // Alert the value of the lastName.
+alert(lastName)
+    // Assign a new value of `"John"` to firstName.
+firstName = "John"
+    // Assign a new value of `"Snow"` to lastName.
+lastName = "Snow"
+    // Alert the new value of `firstName` and `lastName` by concatenating them
+let newValue = firstName + " " + lastName
+    // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+const fullName = firstName + " " + lastName
+    // Declare three variables `name,` `newUser` and `isAdmin`.
+var name = "piku";
+var newUser = "priyanka";
+var isAdmin = "admin";
 // Assign your name as the value of `name`.
-// Assign the value of `name` to `newUser`.
+var name = "piku"
+    // Assign the value of `name` to `newUser`.
+var newUser = "priyanka";
 // Set the value of `isAdmin` to `true`.
+var isAdmin = "true";
 // Alert the values of all the three variables.
-
-// Assignment and re-assignment
+alert(isAdmin)
+alert(newUser)
+alert(name)
+    // Assignment and re-assignment
 
 var wiseMan = "Tyrion Lannister";
 
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
+var wiseMan = "Samwell Tarly";
 // Declare a variable `userName` with value "Lysa Arryn"
-// Declare a variable named `oddNumber` and assign a value `57`.
+var userName = "Lysa Arryn"
+    // Declare a variable named `oddNumber` and assign a value `57`.
+var oddNumber = 57;
 // Reassign the value of `oddNumber` to 61
-
+var oddNumber = 61;
 // Mathematical Operation
 
 let amount = 4280;
@@ -36,6 +52,10 @@ let amount = 4280;
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+var reducedAmount = amount - 24;
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+var addedAmount = amount + 32;
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+var multipleAmount = amount * 7;
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+var dividedAmount = amount / 57;
